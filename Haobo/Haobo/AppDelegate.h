e2730669eb7bdf5601b7e2dcae94dd7966bc46d5
zhb1991nm <826071448@qq.com>
2015-04-28 20:31:33 +0800

    first commit

---
+++ Haobo/Haobo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Haobo
+//
+//  Created by 张浩波 on 15/4/28.
+//  Copyright (c) 2015年 zhb1991nm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Haobo/Haobo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Haobo
+//
+//  Created by 张浩波 on 15/4/28.
+//  Copyright (c) 2015年 zhb1991nm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
